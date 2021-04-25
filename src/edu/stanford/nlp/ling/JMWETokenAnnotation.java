420ca9f527e7f8d004deccca2360d02fd2affae0
Sakae Mizuki <mizuki.s.aa@m.titech.ac.jp>
2021-04-25 21:45:04 +0900

    feat: extended JMWE class so that it will support several methods.

--- src/edu/stanford/nlp/ling/JMWETokenAnnotation.java
+++ src/edu/stanford/nlp/ling/JMWETokenAnnotation.java
@@ -1,23 +1,59 @@
+
 package edu.stanford.nlp.ling;
+import edu.mit.jmwe.data.IMWE;
+import edu.mit.jmwe.data.IToken;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mbelousov on 28/11/2016.
+ * Updated by S.Mizuki on 25/04/2021.
  */
 public class JMWETokenAnnotation implements CoreAnnotation<String> {
     private String mwe_form = null;
+    private IMWE<IToken> mwetoken = null;
 
-    public String getMWEForm() {
-        return this.mwe_form;
+    public JMWETokenAnnotation(IMWE<IToken> itoken){
+        this.mwetoken = itoken;
+        this.mwe_form = itoken.getForm();
     }
 
-    public void setMWEForm(String value) {
-        this.mwe_form = value;
+    public String get_lemmatized_form() {
+        return this.mwetoken.getEntry().getForm();
     }
 
-    @SuppressWarnings("unchecked")
+    public List<String> get_stem_forms() {
+        List<String> lst_ret = new ArrayList<>();
+        for (IToken itoken : this.mwetoken.getTokens()){
+            lst_ret.addAll(itoken.getStems());
+        }
+        return lst_ret;
+    }
+
+    public List<String> get_surface_forms() {
+        List<String> lst_ret = new ArrayList<>();
+        for (IToken itoken : this.mwetoken.getTokens()){
+            lst_ret.add(itoken.getForm());
+        }
+        return lst_ret;
+    }
+
+    public String get_part_of_speech(){
+        return this.mwetoken.getEntry().getPOS().toString();
+    }
+
+    @Override
     public Class<String> getType() {
         return String.class;
     }
 
-
+    @Override
+    public String toString() {
+        return  "surfaces=" + this.get_surface_forms() + '\\'
+                + "stems=" + this.get_stem_forms() + '\\'
+                + "lemma=" + this.get_lemmatized_form() + '\\'
+                + "pos=" + this.get_part_of_speech() + '\\'
+                ;
+    }
 }
